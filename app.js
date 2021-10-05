4cf97fe579235149a163c5534bc0a883de223b04
Pedro Santana <pedromanu27@gmail.com>
2021-10-04 22:07:57 -0300

    post, put, delete

--- app.js
+++ app.js
@@ -20,8 +20,28 @@ const usuarios = [
     }
 ];
 
+app.use(express.json());
 
 app.get('/usuarios', (req, res) => {
+    res.send(usuarios);
+});
+
+app.post('/usuarios/adicionar', (req, res) => {
+    usuarios.push(req.body);
+
+    res.send(usuarios);
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    let usuario = usuarios.find(u => u.id == req.params.id);
+    usuario.nome = req.body.nome
+
+    res.send(usuarios);
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    let i = usuarios.findIndex(u => u.id == req.params.id);
+    usuarios.splice(i, 1);
     res.send(usuarios);
 })
 
